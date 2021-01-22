4faa221aae10e60be681478ce9c943aa35aca873
okuguchihonoka <e1607@s.akashi.ac.jp>
2021-01-22 18:26:42 +0900

    仮

--- RNN.py
+++ RNN.py
@@ -27,7 +27,7 @@ csv300 = np.delete(csv300, 0, 0)
 
 # %%
 #データを格納、学習に使う長さを指定
-length = 301
+length = 301 
 
 data = [] #入力値
 target = []  #出力値
@@ -58,14 +58,12 @@ print(x_train.shape)
 # %%
 #入力、隠れ、出力のユニット数
 l_in = len(x[0])  #301
-l_hidden = 100
+l_hidden = 80
 l_out = 3
 # %%
 #モデルの構築
 model = Sequential()  #入力と出力が１つずつ
-model.add(SimpleRNN(l_hidden,input_shape=(301,1)))
-#model.add(SimpleRNN(l_hidden, input_shpae=(301, ))) #隠れ層のユニット数、活性化関数、入力の形
-#model.add(SimpleRNN(l_hidden,activation='relu',input_dim=3))
+model.add(SimpleRNN(l_hidden,input_shape=(301,1))) #隠れ層のユニット数、活性化関数、入力の形
 model.add(Dense(l_out,activation='softmax')) #多クラス分類なのでソフトマックス関数、シグモイドも試す？
 model.summary()
 
@@ -78,8 +76,22 @@ model.compile(loss='categorical_crossentropy',optimizer=optimizer,metrics=['accu
 #%%
 #学習開始
 batch_size = 64
-epochs = 300
+epochs = 200
 result = model.fit(x_train,t_train, batch_size=batch_size,epochs=epochs,validation_data=(x_valid, t_valid))
+
+#%%
+#過学習チェック
+pp = PdfPages('rnn_accuracy.pdf')
+plt.plot(range(1, epochs+1), result.history['accuracy'], label="train_acc")
+plt.plot(range(1, epochs+1), result.history['val_accuracy'], label="valid_acc")
+plt.title('model accuracy')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend()
+pp.savefig()
+pp.close()
+
+
 # %%
 #学習結果の可視化
 pp = PdfPages('rnn_loss.pdf')
