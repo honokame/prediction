c9c894606f230f2258fc5f7cae89da8f1d640413
okuguchihonoka <e1607@s.akashi.ac.jp>
2021-01-25 01:45:19 +0900

    変更前

--- RNN.py
+++ RNN.py
@@ -16,9 +16,9 @@ from keras.utils import plot_model
 #%%
 #csvファイル読み込み
 #BOM付きなのでencoding="utf_8_sig"を指定
-csv100 = np.loadtxt("/home/honoka/research/prediction/csv/100_1.csv", delimiter=",", encoding='utf_8_sig',unpack=True)
-csv200 = np.loadtxt("/home/honoka/research/prediction/csv/300_1.csv", delimiter=",", encoding='utf_8_sig',unpack=True)
-csv300 = np.loadtxt("/home/honoka/research/prediction/csv/500_1.csv", delimiter=",", encoding='utf_8_sig', unpack=True)
+csv100 = np.loadtxt("/home/honoka/research/prediction/csv/300_1.csv", delimiter=",", encoding='utf_8_sig',unpack=True)
+csv200 = np.loadtxt("/home/honoka/research/prediction/csv/500_1.csv", delimiter=",", encoding='utf_8_sig',unpack=True)
+csv300 = np.loadtxt("/home/honoka/research/prediction/csv/700_1.csv", delimiter=",", encoding='utf_8_sig', unpack=True)
 
 #時間の行を削除
 csv100 = np.delete(csv100, 0, 0)
@@ -27,7 +27,7 @@ csv300 = np.delete(csv300, 0, 0)
 
 # %%
 #データを格納、学習に使う長さを指定
-length = 301 
+length = 101 
 
 data = [] #入力値
 target = []  #出力値
@@ -47,23 +47,21 @@ for i in range(csv300.shape[0]):
 # %%
 #学習できる形に変換
 x = np.array(data)
-x = np.array(data).reshape(600,301,1)
+x = np.array(data).reshape(600,101,1)
 t = np.array(target).reshape(len(target), 1)
 t = np_utils.to_categorical(t)
-print(x.shape)
-print(t.shape)
+
 x_train, x_test, t_train, t_test = train_test_split(x, t, test_size=int(len(data) * 0.2))
 x_valid, x_test, t_valid, t_test = train_test_split(x_test, t_test, test_size=int(len(x_test) * 0.5))
-print(x_train.shape)
 # %%
 #入力、隠れ、出力のユニット数
 l_in = len(x[0])  #301
-l_hidden = 80
+l_hidden = 40
 l_out = 3
 # %%
 #モデルの構築
 model = Sequential()  #入力と出力が１つずつ
-model.add(SimpleRNN(l_hidden,input_shape=(301,1))) #隠れ層のユニット数、活性化関数、入力の形
+model.add(SimpleRNN(l_hidden,input_shape=(101,1))) #隠れ層のユニット数、活性化関数、入力の形
 model.add(Dense(l_out,activation='softmax')) #多クラス分類なのでソフトマックス関数、シグモイドも試す？
 model.summary()
 
@@ -75,8 +73,8 @@ model.compile(loss='categorical_crossentropy',optimizer=optimizer,metrics=['accu
 
 #%%
 #学習開始
-batch_size = 64
-epochs = 150
+batch_size = 32
+epochs = 40
 result = model.fit(x_train,t_train, batch_size=batch_size,epochs=epochs,validation_data=(x_valid, t_valid))
 
 #%%
@@ -122,7 +120,9 @@ for i in range(len(score_test)):
   if (np.argmax(score_test[i]) == np.argmax(t_test[i])):
     count_test += 1
 
+print(epochs,l_hidden,batch_size)
 print("train_acc=")
 print(count_train / len(score_train))
 print("test_acc=")
 print(count_test / len(score_test))
+# %%
